b45dcbf2faf00308829b71ba0f6210e206dba40c
Tim Rogers <tim@gocardless.com>
2012-10-09 11:52:58 +0100

    Ends versioned namespacing to datastore.rb with redis-namespace

--- lib/datastore.rb
+++ lib/datastore.rb
@@ -1,19 +1,21 @@
 require 'redis'
+require 'redis-namespace'
 
 module UCAS
   class Datastore
     @@redis = Redis.new
+    @@adapter = Redis::Namespace.new("ucas-#{UCAS::Application.version}".to_sym, :redis => @@redis)
     
     def self.redis
-      @@redis
+      @@adapter
     end
     
     def self.get(key)
-      @@redis.get(key)
+      @@adapter.get(key)
     end
     
     def self.set(key, value)
-      @@redis.set(key, value)
+      @@adapter.set(key, value)
     end
   end
 end
